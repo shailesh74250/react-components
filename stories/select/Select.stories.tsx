41aa2b472301f5bde5a674060cca8bae2d883dd1
shailesh-doctena <127737937+shailesh-doctena@users.noreply.github.com>
2024-12-18 04:50:19 +0530

    wip

--- stories/select/Select.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Select } from './Select';
-
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
-export default {
-  title: 'Example/Select',
-  component: Select,
-  argTypes: {
-  },
-};
